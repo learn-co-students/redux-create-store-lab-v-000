d2750353a485579683f31213c44d1e79370a618f
Ann Lee <aplee29@users.noreply.github.com>
2017-04-21 00:36:51 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,26 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  // Takes in an action, updates the state using the reducer,
+  // calls the render function
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    // Returns the current state
+    return state;
+  }
+
+  // Dispatches an action that returns the default state
+  dispatch({ type: '@@INIT' })
+
+  // Returns an object with two methods
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
