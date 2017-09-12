08dabb0da8f5d8c917adbc8b928dbec76f2f6801
Isis Tejeda <isis@isistejeda.com>
2017-09-11 21:03:38 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,26 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action){ //dispatch is now private , function needs to be catched when an event 
+                            // occurs. so it is put in a method, here called createStore
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state;
+  };
+
+  dispatch({ type: '@@INIT' });
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+//let store = createStore(someReduce)
