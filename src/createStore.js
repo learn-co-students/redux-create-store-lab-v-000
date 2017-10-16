1cf10c3bc3d1c2fc620fcc3f332235d44c801b6a
christinechoi <smi77y@gmail.com>
2017-10-16 18:49:14 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,20 @@
 export default function createStore(reducer) {
   // add your code here
+
+  function dispatch(action) {
+    countReducer(state, action); 
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  };
+
 }
 
 function render() {
