1eb349b211d7f879a9a27a044a67c0b09a15dbbf
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-17 20:40:13 -0500

    added comments

--- src/createStore.js
+++ src/createStore.js
@@ -1,11 +1,20 @@
 export default function createStore(reducer) {
+  /* the new method:
+    encapsulates the state,
+    holds dispatch
+  */
+
+  //wrapping state in a function
   let state
 
+  //call dispatch with an action, it calls reducer and returns a new state
+  //state is now accessible to dispatch
   function dispatch(action) {
     state = reducer(state, action)
     render()
   }
 
+  //return the state
   function getState() {
     return state
   }
