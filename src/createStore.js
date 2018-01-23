5a87f5af5378152d776343975f53767338688533
jlcobos <luiscobosj@gmail.com>
2018-01-23 14:31:11 -0800

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,20 @@
-export default function createStore(reducer) {
-  // add your code here
+import candyReducer from './reducers/candyReducer'
+import countReducer from './reducers/countReducer'
+
+export default function createStore (reducer) {
+  let state
+
+  function dispatch (action) {
+    state = reducer(state, action)
+    render()
+  }
+  function getState () {
+    return state
+  }
+  dispatch({type: '@@INIT'});
+  return {dispatch, getState}
 }
 
-function render() {
-  const container = document.getElementById('container');
+function render () {
+  const container = document.getElementById('container');;
 }
