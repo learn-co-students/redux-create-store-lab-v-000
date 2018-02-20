9c5c1fddfe691e76b6618a928cd3e1cc9d1f7263
emorgan05 <emorgan05@gmail.com>
2018-02-20 14:29:56 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,49 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  dispatch({type: '@@INIT'});
+  
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+// function candyReducer(state = [], action) {
+//   switch (action.type) {
+//     case 'ADD_CANDY':
+//       return [...state, action.candy];
+//     default:
+//       return state;
+//   }
+// }
+//
+// let candyStore = createStore(candyReducer);
+// candyStore.dispatch({type: '@@INIT'});
+//
+// function countReducer(state = 0, action) {
+//   switch (action.type) {
+//     case 'INCREMENT_COUNT':
+//       return state + 1;
+//     case 'DECREMENT_COUNT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+//
+// let countStore = createStore(countReducer);
+// countStore.dispatch({type: '@@INIT'});
