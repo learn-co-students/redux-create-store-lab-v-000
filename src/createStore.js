e28af443e6aaab3125d469e3ddf8dd800184912a
emorgan05 <emorgan05@gmail.com>
2018-02-20 14:30:20 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -11,7 +11,7 @@ export default function createStore(reducer) {
   }
 
   dispatch({type: '@@INIT'});
-  
+
   return {
     dispatch,
     getState
@@ -21,29 +21,3 @@ export default function createStore(reducer) {
 function render() {
   const container = document.getElementById('container');
 }
-
-// function candyReducer(state = [], action) {
-//   switch (action.type) {
-//     case 'ADD_CANDY':
-//       return [...state, action.candy];
-//     default:
-//       return state;
-//   }
-// }
-//
-// let candyStore = createStore(candyReducer);
-// candyStore.dispatch({type: '@@INIT'});
-//
-// function countReducer(state = 0, action) {
-//   switch (action.type) {
-//     case 'INCREMENT_COUNT':
-//       return state + 1;
-//     case 'DECREMENT_COUNT':
-//       return state - 1;
-//     default:
-//       return state;
-//   }
-// }
-//
-// let countStore = createStore(countReducer);
-// countStore.dispatch({type: '@@INIT'});
