2bd09ef73b8f7f67f377243d9538c74ee8c393a1
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-05-17 23:22:57 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,23 @@
+
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function getState() {
+    return state;
+  }
+
+  dispatch( {type: '@@INIT'})
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render();
+  }
+
+  return {
+    getState,
+    dispatch
+  }
 }
 
 function render() {
