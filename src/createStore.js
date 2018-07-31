d94a1e066756a16d2376bb1b5c5648602663baae
paaaarv <pkumar@trinity.edu>
2018-07-31 18:21:09 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,28 @@
+
+let store, button;
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+
+  function getState() {
+    return state
+  }
+
+  function dispatch(action){
+    state = reducer(state, action)
+    if (store) render()
+
+  }
+
+  dispatch({type: "@@INIT"})
+  return {
+    dispatch,
+    getState}
+
+
 }
 
 function render() {
   const container = document.getElementById('container');
+  container.textContent = store.getState();
 }
