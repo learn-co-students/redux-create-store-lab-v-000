631d957476e77c72342e764213c88ad009f960d0
mrfarmer7771 <mfarmer@d120.org>
2018-08-12 17:20:17 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,20 +1,23 @@
 export default function createStore(reducer) {
   // add your code here
   let state;
+  //Have the supplied reducer run its '@@INIT' action to initialize the state
   state=reducer(state,{type: "@@INIT"})
   
-  
+  //Set up the agnostic dispatch function using the provided reducer
   function dispatch(action){
     state=reducer(state,action);
-    render();
+    render();  //This was provided below for this lab
   }
   
+  
+  //Returns the state for use in other functions
   function getState(){
     return state;
   }
   
   
-  
+  //Returns an object with the dispatch and getState functions written above, will turn into 'methods' for this store object
   return(
     {dispatch, getState}
   )
