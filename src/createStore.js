819ce5ea9cb76d0d9be394607f6aece142b85458
mrfarmer7771 <mfarmer@d120.org>
2018-08-12 17:18:30 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,27 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+  state=reducer(state,{type: "@@INIT"})
+  
+  
+  function dispatch(action){
+    state=reducer(state,action);
+    render();
+  }
+  
+  function getState(){
+    return state;
+  }
+  
+  
+  
+  return(
+    {dispatch, getState}
+  )
+  
+  
+  
+  
 }
 
 function render() {
