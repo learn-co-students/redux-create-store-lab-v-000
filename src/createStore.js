6050fa39f74b53da28a4b531ce9a0b282670b974
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-27 20:34:01 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,29 @@
 export default function createStore(reducer) {
-  // add your code here
+
+  let state = 0
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState(){
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 }
 
 function render() {
