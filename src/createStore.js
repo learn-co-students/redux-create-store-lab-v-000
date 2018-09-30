2369a80f0672880da8c5d12863b7f4b755777e7c
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-30 14:32:01 -0700

    init commit

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,22 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  dispatch({ type: '@@INIT' });
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    getState,
+    dispatch
+  };
 }
 
 function render() {
