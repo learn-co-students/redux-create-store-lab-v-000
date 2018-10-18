957510cf129a5fdaca67ecd527ac075b4ed1b4b4
Courtney <chalt0319@gmail.com>
2018-10-18 10:54:54 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,3 @@
-import candyReducer from './reducers/candyReducer'
-import countReducer from './reducers/countReducer'
 
 export default function createStore(reducer) {
   // add your code here
@@ -15,6 +13,8 @@ export default function createStore(reducer) {
     return state
   }
 
+  dispatch({ type: '@@init'})
+
   return {dispatch, getState}
 
 }
@@ -22,9 +22,3 @@ export default function createStore(reducer) {
 function render() {
   const container = document.getElementById('container');
 }
-
-// let storeCount = createStore(countReducer)
-// createStore(storeCount).dispatch({type: "not"})
-//
-// let storeCandy = createStore(candyReducer)
-// createStore(storeCandy).dispatch({type: "not"})
