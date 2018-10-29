8841c41e0f71484139f1078d37a778d38920aece
Emi Ko <emi8emi@gmail.com>
2018-10-29 11:50:36 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,21 @@
 export default function createStore(reducer) {
   // add your code here
-}
 
-function render() {
+    let state;
+
+    function dispatch(action) {
+    state = reducer(state, action)
+    render()
+    }
+
+    const getState = () => state
+
+    dispatch({ type: '@@INIT' })
+
+    return {dispatch, getState}
+    }
+
+
+  function render() {
   const container = document.getElementById('container');
-}
+  }
