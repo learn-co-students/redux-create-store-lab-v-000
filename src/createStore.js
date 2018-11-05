cb84f51b6d7aa4a9427d1d3b10fe84be84306fb7
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-05 21:32:04 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,19 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+   function getState() {
+    return state;
+  }
+   function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+   // dispatch an initialize action to get state set
+  dispatch({ type: '@@INIT' })
+   return {
+    getState,
+    dispatch
+  };
 }
 
 function render() {
