9a0c2c49104a8385b83d54aaca94e90457e511e8
Jrodr4544 <maskdbandit@gmail.com>
2018-11-05 17:21:36 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,25 @@
 export default function createStore(reducer) {
   // add your code here
-}
+  let state;
+
+  function dispatch(action) {
+        state = reducer(state, action);
+        render();
+   }
+
+  function getState() {
+        return state;
+  }
+
+  dispatch({type: '@@init'})
+
+  return {
+	getState,
+	dispatch
+  };
+};
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById('main');
 }
+
