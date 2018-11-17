0fa7289ceaad0c4ec2b7f2f984b76ca0156e3e04
github username <schun08@gmail.com>
2018-11-17 15:12:11 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,22 +1,48 @@
+// first attempt 5/22/18
+// export default function createStore(reducer) {
+//   let state;
+//
+//   function dispatch(action){
+//     state = reducer(state, action);
+//     render();
+//   }
+//
+//   function getState(){
+//     return state;
+//   }
+//
+//   dispatch({type: '@@INIT'});
+//
+//   return {
+//     dispatch,
+//     getState
+//   };
+// }
+//
+// function render() {
+//   const container = document.getElementById('container');
+// }
+
+// second attempt 11/17/18
+
 export default function createStore(reducer) {
   let state;
 
-  function dispatch(action){
+  function dispatch(action) {
     state = reducer(state, action);
     render();
-  }
+  };
 
-  function getState(){
+  function getState() {
     return state;
   }
-
   dispatch({type: '@@INIT'});
-  
+
   return {
     dispatch,
     getState
   };
-}
+};
 
 function render() {
   const container = document.getElementById('container');
