5ed23a6a844dc26eeb38169aa7c27e49aa4de8d7
tsyaeger <tsyaeger@gmail.com>
2018-11-24 14:26:20 -0800

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -2,6 +2,28 @@ export default function createStore(reducer) {
   // add your code here
 }
 
+
+function createStore(reducer){
+	let state;
+
+	function dispatch(action){
+		state = reducer(state, action)
+		render()
+	}
+
+	function getState(){
+		return state
+	}
+
+	return {
+		dispatch,
+		getState
+	}
+}
+
+
+
+
 function render() {
   const container = document.getElementById('container');
 }
