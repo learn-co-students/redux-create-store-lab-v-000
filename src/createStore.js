ae1f26b94401346d06c9f0d59abfc018b4821c1a
github username <github email address>
2019-02-10 22:20:57 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,32 @@
-export default function createStore(reducer) {
-  // add your code here
-}
 
-function render() {
-  const container = document.getElementById('container');
+export default function createStore(reducer,initialState) {
+  let state;
+  // const store = {};
+  // store.state = ;
+  // store.listeners = [];
+  //
+  // store.getState = () => store.state
+  // store.dispatch = (action) => {
+  //   store.state = reducer(store.state, action)
+  //
+  // }
+  // store.subscribe = (listener) => {
+  //   store.listeners.push(listener);
+  // }
+
+  function dispatch(action){
+    state = reducer(state, action)
+    // store.listeners.forEach(listener => listener())
+  }
+
+  function setState(reducer){
+
+    this.state = state;
+  }
+  function getState(){
+    return state
+  }
+  dispatch({})
+
+  return {dispatch, getState}
 }
