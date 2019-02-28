95780efb34ad284c5bb2056377809dc9430f6e1c
github username <github email address>
2019-02-27 21:43:57 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,33 @@
+import countReducer from './reducers/countReducer.js';
+import candyReducer from './reducers/candyReducer.js';
+
 export default function createStore(reducer) {
-  // add your code here
-}
+
+
+    let state;
+
+    function dispatch(action) {
+      state = reducer(state, action);
+      render();
+    }
+
+    function getState() {
+      return state;
+    };
+
+    dispatch({ type: '@@INIT' });
+
+    return {
+      dispatch,
+      getState
+    };
+  };
+
 
 function render() {
-  const container = document.getElementById('container');
+//  const container = document.getElementById('container');
 }
+
+//let store = createStore(countReducer)
+
+//let button = document.getElementById('button');
