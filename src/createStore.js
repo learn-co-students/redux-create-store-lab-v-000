1e053bd962c40d101fe8176ea7dad5c02b1176fb
pracniscate <lorena.giddens@outlook.com>
2019-03-26 21:34:31 -0400

    done

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,27 @@
+import candyReducer from "./reducers/candyReducer";
+
 export default function createStore(reducer) {
-  // add your code here
-}
+  let state;
+
+  function dispatch(action) {
+    // update state using reducer
+    state = reducer(state, action);
+    // call render()
+    render();
+  };
+
+  function getState() { // get current state
+    return state;
+  };
+
+  dispatch({ type: '@@INIT' });
+
+  return {
+    getState,
+    dispatch
+  };
+};
 
 function render() {
   const container = document.getElementById('container');
-}
+};
