a25fd1867bf898504c5bc48e97f81ea1b3169689
Rollin <rollin.metzger@gmail.com>
2019-05-07 17:26:40 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,26 @@
 export default function createStore(reducer) {
-  // add your code here
+  // add our state var here so it can be accessed by dispatch()
+  let state;
+
+  // we need to persist the change reducer makes to state
+  // remember, the reducer needs to include an action, as we can see in /reducers
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  // we'll populate the initial state tree with the abbreviated ActionTypes.INIT
+  dispatch({ type: '@@INIT' });
+
+  // we return getState()
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
