35015a6700fc3a489503f9cca889a03fc6fe7887
lauraBo <BodellLaura@gmail.com>
2019-05-09 13:13:32 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,29 @@
 export default function createStore(reducer) {
   // add your code here
-}
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  };
+
+  dispatch({ type: '@@INIT' });
+
+  return {
+    dispatch,
+    getState
+  };
+
+};
 
 function render() {
   const container = document.getElementById('container');
 }
+
+
+
+//You'll have to dispatch some sort of 'initialize' action to get your state set!
