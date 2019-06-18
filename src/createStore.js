7635298e33ac44c9d6678bb3bfaba0b5f67ec386
roitblatari <roitblatari@gmail.com>
2019-06-17 23:23:31 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,26 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+	// add your code here
+	let state;
+	// function getState() {
+	// 	return reducer.state;
+	// }
 
+	function dispatch(action) {
+		state = reducer(state, action);
+		render();
+	}
+
+	function getState() {
+		return state;
+	}
+
+	return {
+		dispatch,
+		getState
+	};
+}
+// let store = createStore(reducer);
+// store.dispatch({ type: '@@INIT' });
 function render() {
-  const container = document.getElementById('container');
+	const container = document.getElementById('container');
 }
