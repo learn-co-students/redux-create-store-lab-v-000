3cd574f87951bcb57ad2c3aa28b641144cf4a0b4
harleyharl <harleymteden@gmail.com>
2019-06-20 12:03:30 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,24 @@
+import candyReducer from './reducers/candyReducer';
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
-function render() {
-  const container = document.getElementById('container');
-}
+// function render() {
+//   let container = document.getElementById('container');
+//   container.textContent = store.getState().count;
+// };
