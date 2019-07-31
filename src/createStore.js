0b67dbadbc6889b0b297eb5969e89cd106f6e24b
sarawall1187 <sarawall1187@gmail.com>
2019-07-31 09:41:24 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,28 @@
+// import './reducers/countReducer.js'
+// import './reducers/candyReducer.js'
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+
+
+  dispatch({ type: '@@INIT' })
+ 
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
