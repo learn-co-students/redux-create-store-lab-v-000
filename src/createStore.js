8b49859f99b2c14d5b1cffd692745ed13edda549
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-19 16:28:38 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -3,9 +3,10 @@
 
 export default function createStore(reducer) {
   let state;
-  
+
   function dispatch(action) {
     state = reducer(state, action)
+    render()
   }
 
   dispatch({ type: '@@INIT'})
@@ -13,36 +14,11 @@ export default function createStore(reducer) {
   function getState() {
     return state
   }
+
   return {dispatch, getState}
 }
-  // let state;
 
-  // function dispatch(action) {
-  //   state = reducer(state, action)
-  //   render()
-  // }
-
-  // function getState() {
-  //   return state
-  // }
-
-  // return {
-  //   dispatch,
-  //   getState
-  // }
-
-
-// function render() {
-//   const container = document.getElementById('container');
-//   container.textContent = store.getState().count
-// }
-
-// let store = createStore(reducer)
-// store.dispatch({ type: '@@INIT'})
-
-// let button = document.getElementById('button');
- 
-// button.addEventListener('click', function() {
-//   store.dispatch({ type: 'INCREMENT_COUNT' });
-// });
+function render() {
+  const container = document.getElementById('container');
+}
 
