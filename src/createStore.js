6ffb97525ee06964a61db96d394b19bfbcab6f4c
Wendy Calderon <wendycalderon@gmail.com>
2019-08-22 10:54:51 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,22 @@
+//createStore takes the reducer as the argument:
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+ 
+  dispatch({ type: '@@INIT' })
+  
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
