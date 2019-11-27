969a63a14494ff953d36a3ff2e6547306d456aa7
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-11-27 16:29:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 // write your createStore function here
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -7,16 +25,18 @@ function candyReducer(state = [], action) {
     default:
       return state;
   }
-}
+};
 
 function render() {
   let container = document.getElementById('container');
-  if(store.getState()) {
-    container.textContent = store.getState().join(' ')
-  } else {
-    throw new Error("the store's state has not been defined yet")
-  }
+    container.textContent = store.getState().join(' ');
 };
 
-// use your createStore function and the functions provided here to create a store
-// once the store is created, call an initial dispatch
+let store = createStore(candyReducer); // createStore takes the reducer reducer as an argument
+  store.dispatch({ type: '@@INIT' });
+
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'ADD_CANDY' });
+});
